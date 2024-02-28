86e12d7d6b199edef5e99a8e06ed8d340a521793
Sergey Khomich <sergii.khomych@geeksforless.com>
2024-02-29 01:36:14 +0200

    refactoring

--- data/postgres/src/main/resources/db/migration/V2__Insert_reference_data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO account (id, first_name, last_name) VALUES
-                                                    ('8cd687cc-07a9-48bc-b560-f566f50c3a09', 'John', 'Doe'),
-                                                    ('d9949b0b-fdb3-424b-a2e8-3d64fbd41e68', 'Jane', 'Smith'),
-                                                    ('9b3b24ef-8f0b-4330-b93a-1f8f4e449e13', 'Alice', 'Johnson');
-INSERT INTO usr (email, name, password) VALUES
-                                            ('test1@example.com', 'test1', '111'),
-                                            ('test2@example.com', 'test2', '111');
